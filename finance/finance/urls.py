8124eaaa8df10c3fe86591ff21480a33848488d5
Zheng Qin <zqin@shutterstock.com>
2014-12-30 20:44:08 -0500

    update url

--- finance/finance/urls.py
+++ finance/finance/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from spending import views as sp_views
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^spending-list/$', sp_views.list_spending),
+    url(r'^spending-new/$', sp_views.new_spending),
+    url(r'^spending-add/$', sp_views.add_spending),
 )
