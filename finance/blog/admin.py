969dd77c04e1a0b1c218bc1bbe8dabebd54765a3
Zheng Qin <zqin@shutterstock.com>
2015-01-05 18:28:34 -0500

    commit before leave

--- finance/blog/admin.py
+++ finance/blog/admin.py
@@ -4,4 +4,8 @@ from blog.models import Category
 
 admin.site.register(Category)
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = { 'slug': ['title'] }
+
 # Register your models here.
